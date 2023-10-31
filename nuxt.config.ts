b61ba0049af6fd700cbc05e873532b39fc4c74e0
himanshu-om-khandelwal <me.himanshu.k@gmail.com>
2023-10-31 21:01:14 +0530

    tailwind dependecy added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,11 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/ui'
   ],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
